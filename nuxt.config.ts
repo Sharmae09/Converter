2e56c8c440a7c1b320809b52d95035fba3881c5d
Sharmae09 <sharmae.code@gmail.com>
2025-06-04 14:47:11 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: false },
   build: {
     transpile: ["vuetify"],
   },
-  ssr: false,
+  nitro: {
+    preset: "static",
+  },
 });
